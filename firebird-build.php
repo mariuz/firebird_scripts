3b86a1ddcc09e642fa43a8a4c0ec294352174e3a
mariuz <mapopa@gmail.com>
2008-08-19 19:21:04 +0300

    added --continue to wget

--- firebird-build.php
+++ firebird-build.php
@@ -3,7 +3,7 @@
 passthru("sudo apt-get install automake libtool libreadline5-dev make btyacc bison gawk g++ wget"); 
 passthru("mkdir -p /opt/build");
 chdir("/opt/build");
-passthru("wget http://downloads.sourceforge.net/firebird/Firebird-2.5.0.20343-Alpha1.tar.bz2");
+passthru("wget --continue  http://downloads.sourceforge.net/firebird/Firebird-2.5.0.20343-Alpha1.tar.bz2");
 passthru("tar -jxvf Firebird-2.5.0.20343-Alpha1.tar.bz2");
 chdir("Firebird-2.5.0.20343-Alpha1");
 passthru("./autogen.sh --prefix=/opt/firebird2.5.x --enable-superserver");
