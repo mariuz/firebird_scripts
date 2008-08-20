7788e738baec08d48329898e6edcb7b16c4c39dc
mariuz <mapopa@gmail.com>
2008-08-20 18:12:20 +0300

    fixed script

--- nginx-build.php
+++ nginx-build.php
@@ -1,5 +1,22 @@
 #!/usr/bin/php
 <?
+
+passthru("sudo cp src/spawn-fcgi /usr/bin/spawn-fcgi");
+$fp = fopen('/usr/bin/php-fastcgi','w');
+fwrite($fp,"#!/bin/sh\n");
+fwrite($fp,"/usr/bin/spawn-fcgi -a 127.0.0.1 -p 9000 -u www-data -f /usr/bin/php5-cgi\n");
+fclose($fp);
+
+passthru("sudo chmod +x /usr/bin/php-fastcgi");
+passthru("cp nginx/init-fastcgi /etc/init.d");
+passthru("chmod 755 /etc/init.d/init-fastcgi");
+passthru("update-rc.d init-fastcgi defaults");
+passthru("/etc/init.d/init-fastcgi start");
+
+
+
+
+
 passthru("sudo apt-get install  php5-cli php5-cgi build-essential libpcre3 libpcre3-dev libpcrecpp0 libssl-dev zlib1g-dev"); 
 passthru("mkdir -p /opt/build/nginx");
 chdir("/opt/build/nginx");
@@ -16,21 +33,6 @@ passthru("tar -zxvf lighttpd-1.4.19.tar.gz");
 chdir("lighttpd-1.4.19.tar.gz");
 passthru("./configure");
 passthru("make");
-passthru("sudo cp src/spawn-fcgi /usr/bin/spawn-fcgi");
-$fp = fopen('/usr/bin/php-fastcgi','w');
-fwrite($fp,"#!/bin/sh\n");
-fwrite($fp,"/usr/bin/spawn-fcgi -a 127.0.0.1 -p 9000 -u www-data -f /usr/bin/php5-cgi\n");
-fclose($fp);
-
-passthru("sudo chmod +x /usr/bin/php-fastcgi");
-passthru("cp nginx/init-fastcgi /etc/init.d");
-passthru("chmod 755 /etc/init.d/init-fastcgi");
-passthru("update-rc.d init-fastcgi defaults");
-passthru("/etc/init.d/init-fastcgi start");
-
-
-
-
 
 
 ?>
