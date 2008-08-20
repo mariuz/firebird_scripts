cec62e98025d8bfb4fdbfbff40608c048817f91e
mariuz <mapopa@gmail.com>
2008-08-20 17:39:23 +0300

    use nginx-0.6.32

--- nginx-build.php
+++ nginx-build.php
@@ -3,8 +3,8 @@
 passthru("sudo apt-get install apt-get install php5-cli php5-cgi build-essential libpcre3 libpcre3-dev libpcrecpp0 libssl-dev zlib1g-dev"); 
 passthru("mkdir -p /opt/build/nginx");
 chdir("/opt/build/nginx");
-passthru("wget --continue  http://sysoev.ru/nginx/nginx-0.7.11.tar.gz");
-passthru("tar -zxvf nginx-0.7.11.tar.gz");
+passthru("wget --continue  http://sysoev.ru/nginx/nginx-0.6.32.tar.gz");
+passthru("tar -zxvf nginx-0.6.32.tar.gz");
 chdir("nginx-0.7.11");
 passthru("./configure --prefix=/opt/nginx --with-http_flv_module");
 passthru("make");
