0ead1a81e3221aab6bebdc3437a770549e6a2e11
mariuz <mapopa@gmail.com>
2008-08-20 11:13:34 +0300

    added firebird 3.0 build script

---
+++ firebird3.x-build.php
@@ -0,0 +1,12 @@
+#!/usr/bin/php
+<?
+passthru("sudo apt-get install automake libtool libreadline5-dev make btyacc bison gawk g++ wget cvs"); 
+passthru("mkdir -p /opt/build/firebird3.x");
+chdir("/opt/build/firebird3.x");
+passthru("cvs -d:pserver:anonymous@firebird.cvs.sourceforge.net:/cvsroot/firebird login");
+passthru("cvs -z3 -d:pserver:anonymous@firebird.cvs.sourceforge.net:/cvsroot/firebird co firebird2");
+chdir("firebird2");
+passthru("./autogen.sh --prefix=/opt/firebird3.x --enable-superserver");
+passthru("make");
+passthru("sudo make install");
+?>
