8d0c16dacae3c00641c7427be4819bef726571cf
mariuz <mapopa@gmail.com>
2008-08-22 15:29:24 +0300

    firebird-install.php script

---
+++ firebird-install.php
@@ -0,0 +1,15 @@
+#!/usr/bin/php
+
+
+
+<?
+
+$fp = fopen('/etc/apt/sources.list.d/firebird.list','w');
+fwrite($fp,"deb http://ppa.launchpad.net/mapopa/ubuntu hardy main\n");
+fwrite($fp,"deb-src http://ppa.launchpad.net/mapopa/ubuntu hardy main\n");
+fclose($fp);
+
+passthru("sudo apt-get update"); 
+passthru("sudo apt-get -y --force-yes install firebird2.1-super php5-interbase");
+passthru("sudo sudo dpkg-reconfigure firebird2.1-super");
+?>
