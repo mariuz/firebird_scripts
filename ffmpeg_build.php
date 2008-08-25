49fee83eb6e571e2d8483c95006197de5e8cb7d7
mariuz <mapopa@gmail.com>
2008-08-25 17:39:26 +0300

    add x264 snapshot

--- ffmpeg_build.php
+++ ffmpeg_build.php
@@ -35,7 +35,8 @@ if (($OperatingSystem == "Ubuntu") && ($Version =="8.04"))
 {
 passthru ('sudo apt-get -y install ruby subversion gcc git-core automake unzip libogg-dev libvorbis-dev wget ');
 passthru ('sudo apt-get -y build-dep ffmpeg');
-passthru ('sudo apt-get -y install liblame-dev libfaad-dev libfaac-dev libxvidcore4-dev liba52-0.7.4 liba52-0.7.4-dev libdts-dev checkinstall liba52-dev libdts-dev libgsm1-dev libvorbis-dev libdc1394-dev libx264-dev libfaad-dev libtheora-dev libsdl1.2-dev ');
+passthru ('sudo apt-get -y install liblame-dev libfaad-dev libfaac-dev libxvidcore4-dev liba52-0.7.4 liba52-0.7.4-dev libdts-dev checkinstall liba52-dev libdts-dev libgsm1-dev libvorbis-dev libdc1394-dev libfaad-dev libtheora-dev libsdl1.2-dev ');
+passthru ('sudo apt-get -y remove libx264-dev libx264');
 
 
 #From http://www.penguin.cz/~utx/amr download amrnb-7.0.0.2.tar.bz2 and amrwb-7.0.0.3.tar.bz2       
@@ -50,11 +51,27 @@ passthru ('sudo tar -jxvf amrwb-7.0.0.3.tar.bz2');
 chdir ('amrwb-7.0.0.3');
 ConfigureMakeInstall();
 
+
+#10.GEt x264 from here  http://www.videolan.org/developers/x264.html
+
+
+passthru('wget ftp://ftp.videolan.org/pub/videolan/x264/snapshots/x264-snapshot-20080625-2245.tar.bz2');
+passthru('tar -jxvf x264-snapshot-20080625-2245.tar.bz2');
+passthru('cd x264-snapshot-20080625-2245');
+passthru('./configure --prefix=/usr --disable-asm --enable-pic ;make ;make install');
+chdir (BUILDDIR);
+
+
+
 #Finally ffmpeg from svn trunk 
 
 passthru ('svn checkout svn://svn.mplayerhq.hu/ffmpeg/trunk ffmpeg');
 passthru ('cd ffmpeg;./configure --prefix=/usr --enable-libamr-nb --enable-libamr-wb --enable-libvorbis --enable-libxvid --enable-liba52 --enable-libmp3lame --enable-libx264 --enable-libtheora --enable-libfaad --enable-gpl --enable-shared --enable-pthreads --enable-libfaac --enable-nonfree --enable-postproc;make ; make install'); 
 chdir (BUILDDIR);
+
+
+
+
 }
 
 
