3073d71eba4710304f1e3a0c93ff379126454550
mariuz <mapopa@gmail.com>
2008-08-25 18:41:21 +0300

    add x264 from git

--- ffmpeg_build.php
+++ ffmpeg_build.php
@@ -33,7 +33,7 @@ chdir (BUILDDIR);
 if (($OperatingSystem == "Ubuntu") && ($Version =="8.04"))
 
 {
-passthru ('sudo apt-get -y install ruby subversion gcc git-core automake unzip libogg-dev libvorbis-dev wget php5-dev ');
+passthru ('sudo apt-get -y install ruby subversion gcc git-core automake unzip libogg-dev libvorbis-dev wget  ');
 passthru ('sudo apt-get -y build-dep ffmpeg');
 passthru ('sudo apt-get -y install liblame-dev libfaad-dev libfaac-dev libxvidcore4-dev liba52-0.7.4 liba52-0.7.4-dev libdts-dev checkinstall liba52-dev libdts-dev libgsm1-dev libvorbis-dev libdc1394-dev libfaad-dev libtheora-dev libsdl1.2-dev ');
 passthru ('sudo apt-get -y remove libx264-dev libx264');
