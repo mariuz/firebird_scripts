eb171d55eb28ac7be782b06e377406c53786b30c
mariuz User <mariuz@ubuntu.karmanet.ro>
2008-09-14 00:46:58 -0700

    added the version for future releases 2.5 beta/rc 3.0

--- firebird-build.php
+++ firebird-build.php
@@ -1,11 +1,12 @@
 #!/usr/bin/php
 <?
+$FirebirdVersion="Firebird-2.5.0.20343-Alpha1";
 passthru("sudo apt-get install automake libtool libreadline5-dev make btyacc bison gawk g++ wget"); 
 passthru("mkdir -p /opt/build");
 chdir("/opt/build");
-passthru("wget --continue  http://downloads.sourceforge.net/firebird/Firebird-2.5.0.20343-Alpha1.tar.bz2");
-passthru("tar -jxvf Firebird-2.5.0.20343-Alpha1.tar.bz2");
-chdir("Firebird-2.5.0.20343-Alpha1");
+passthru("wget --continue  http://downloads.sourceforge.net/firebird/$FirebirdVersion.tar.bz2");
+passthru("tar -jxvf $FirebirdVersion.tar.bz2");
+chdir("$FirebirdVersion");
 passthru("./autogen.sh --prefix=/opt/firebird2.5.x --enable-superserver");
 passthru("make");
 passthru("sudo make install");
