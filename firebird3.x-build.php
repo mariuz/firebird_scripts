8cef039d74fd1dddcef9e8af6042a006a57cc438
mariuz User <mariuz@ubuntu.karmanet.ro>
2008-09-14 00:49:26 -0700

    added an comment

--- firebird3.x-build.php
+++ firebird3.x-build.php
@@ -1,5 +1,6 @@
 #!/usr/bin/php
 <?
+#This Builds the latest firebird from head
 passthru("sudo apt-get install automake libtool libreadline5-dev make btyacc bison gawk g++ wget cvs"); 
 passthru("mkdir -p /opt/build/firebird3.x");
 chdir("/opt/build/firebird3.x");
