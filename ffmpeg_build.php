b2ec22d650db5bafe3cec7f40f3a882498b14630
Marius Popa <mapopa@gmail.com>
2008-09-23 01:54:08 +0300

    added condition for ffmpeg on intrepid/hardy

--- ffmpeg_build.php
+++ ffmpeg_build.php
@@ -1,8 +1,10 @@
 #!/usr/bin/php
 <?php
 
+$UbuntuRelease = exec("lsb_release -c -s");
+echo $UbuntuRelease;
+
 $OperatingSystem="Ubuntu";
-$Version="8.04";
 define("BUILDDIR","/opt/build/");
 
 function Configure()
@@ -30,23 +32,26 @@ chdir (BUILDDIR);
 // see this page also https://wiki.ubuntu.com/ffmpeg
 
 
-if (($OperatingSystem == "Ubuntu") && ($Version =="8.04"))
-
-{
+if ($OperatingSystem == "Ubuntu") {
 passthru ('apt-get -y install ruby subversion gcc git-core automake unzip libogg-dev libvorbis-dev wget  ');
 passthru ('apt-get -y build-dep ffmpeg');
-passthru ('apt-get -y install liblame-dev libfaad-dev libfaac-dev libxvidcore4-dev liba52-0.7.4 liba52-0.7.4-dev libdts-dev checkinstall liba52-dev libdts-dev libgsm1-dev libvorbis-dev libdc1394-dev libfaad-dev libtheora-dev libsdl1.2-dev ');
+passthru ('apt-get -y install libfaad-dev libfaac-dev libxvidcore4-dev liba52-0.7.4-dev libdts-dev checkinstall liba52-dev libdts-dev libgsm1-dev libvorbis-dev libdc1394-dev libfaad-dev libtheora-dev libsdl1.2-dev ');
 passthru ('apt-get -y remove libx264-dev libx264');
+
+if ( $UbuntuRelease =="8.04")
+	{
+
+	passthru ('apt-get -y install liblame-dev');
+
+	}
+
+if ( $UbuntuRelease  =="8.10")
+	{
+	#on intrepid 
+	passthru ('apt-get -y install libmp3lame-dev');
+
+	}
 }
-
-else 
-
-{
-#on intrepid 
-passthru ('apt-get -y install libmp3lame-dev liba52-0.7.4-dev libfaac-dev libxvidcore4-dev');
-
-}
-
 
 #From http://www.penguin.cz/~utx/amr download amrnb-7.0.0.2.tar.bz2 and amrwb-7.0.0.3.tar.bz2       
 
