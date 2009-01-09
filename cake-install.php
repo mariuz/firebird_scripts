31c03a0a286ca81fe48bddb627e0393857a3f7ca
mariuz <mapopa@gmail.com>
2009-01-09 19:09:28 +0200

    added cake version

--- cake-install.php
+++ cake-install.php
@@ -1,10 +1,10 @@
 #!/usr/bin/php
 <?
-
+$cake_version="1.2.0.7962";
 // Clean cake php 
 chdir("/opt/build/");
-passthru("wget --continue  http://cakeforge.org/frs/download.php/637/cake_1.2.0.7296-rc2.tar.bz2/donation=complete");
-passthru("mv donation\=complete cake_1.2.0.7296-rc2.tar.bz2 ; tar -jxvf cake_1.2.0.7296-rc2.tar.bz2");
-passthru("chown -R www-data.www-data cake_1.2.0.7296-rc2");
-passthru("mv cake_1.2.0.7296-rc2 /opt/nginx/html/firetube ");
+passthru("wget http://cakeforge.org/frs/download.php/694/$cake_version.tar.bz2/donation=complete -O cake-$cake_version.tar.bz2");
+passthru("tar -jxvf cake-$cake_version.tar.bz2");
+passthru("chown -R www-data.www-data cake_$cake_version");
+passthru("mv cake_$cake_version /opt/nginx/html/firetube ");
 ?>
