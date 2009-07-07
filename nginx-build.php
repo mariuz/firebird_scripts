9f5bbe36bb0a620390b2b0347fbeaf033015843b
mariuz <mapopa@gmail.com>
2009-07-07 15:03:27 -0700

    added NGINX_VERSION constant

--- nginx-build.php
+++ nginx-build.php
@@ -1,6 +1,6 @@
 #!/usr/bin/php
 <?
-
+DEFINE(NGINX_VERSION,'nginx-0.7.61');
 $fp = fopen('/usr/bin/php-fastcgi','w');
 fwrite($fp,"#!/bin/sh\n");
 fwrite($fp,"/usr/bin/spawn-fcgi -a 127.0.0.1 -p 9000 -u www-data -f /usr/bin/php5-cgi\n");
@@ -21,9 +21,9 @@ passthru("update-rc.d nginx defaults");
 passthru("apt-get install -y php5-cli php5-cgi build-essential libpcre3 libpcre3-dev libpcrecpp0 libssl-dev zlib1g-dev wget"); 
 passthru("mkdir -p /opt/build/nginx");
 chdir("/opt/build/nginx");
-passthru("wget --continue  http://sysoev.ru/nginx/nginx-0.6.32.tar.gz");
-passthru("tar -zxf nginx-0.6.32.tar.gz");
-chdir("nginx-0.6.32");
+passthru("wget --continue  http://sysoev.ru/nginx/$NGINX_VERSION.tar.gz");
+passthru("tar -zxf $NGINX_VERSION.tar.gz");
+chdir("$NGINX_VERSION");
 passthru("./configure --prefix=/opt/nginx --with-http_flv_module");
 passthru("make");
 passthru("make install");
