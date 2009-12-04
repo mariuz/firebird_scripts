7a133648d49f4e302d5a6a0e4b86c56a22f7e4f3
mariuz <mapopa@gmail.com>
2009-12-04 08:52:21 -0700

    by default spwn-fcgi creates only one child

--- nginx-build.php
+++ nginx-build.php
@@ -5,7 +5,7 @@ DEFINE(SPAWN_FCGI_VERSION,'spawn-fcgi-1.6.2');
 
 $fp = fopen('/usr/bin/php-fastcgi','w');
 fwrite($fp,"#!/bin/sh\n");
-fwrite($fp,"/usr/bin/spawn-fcgi -a 127.0.0.1 -p 9000 -u www-data -f /usr/bin/php5-cgi\n");
+fwrite($fp,"/usr/bin/spawn-fcgi -C 10 -F 10 -a 127.0.0.1 -p 9000 -u www-data -f /usr/bin/php5-cgi\n");
 fclose($fp);
 
 passthru("chmod +x /usr/bin/php-fastcgi");
