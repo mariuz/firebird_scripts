2b12912b0deb69a45a6c12e798e0152bce20de2d
Popa Adrian Marius (Mariuz) <mapopa@gmail.com>
2011-09-22 16:38:52 +0300

    scripts cleanup , get rid of flash tools , added support for php5-fpm (seems to be faster)

--- nginx-build.php
+++ nginx-build.php
@@ -2,19 +2,18 @@
 <?
 DEFINE(NGINX_VERSION,'nginx-1.0.6');
 
-passthru("chmod +x /usr/bin/php-fastcgi");
 passthru("mkdir -p /opt/nginx/conf");
-passthru("cp nginx/init-fastcgi /etc/init.d");
 passthru("cp nginx/nginx /etc/init.d");
 passthru("cp nginx/nginx.conf /opt/nginx/conf");
-passthru("chmod 755 /etc/init.d/init-fastcgi");
+passthru("cp nginx/mime.types /opt/nginx/conf");
 passthru("chmod 755 /etc/init.d/nginx");
-passthru("update-rc.d init-fastcgi defaults");
 passthru("update-rc.d nginx defaults");
 
 
 
-passthru("apt-get install -y php5-cli php5-cgi build-essential libpcre3 libpcre3-dev libpcrecpp0 libssl-dev zlib1g-dev wget apt-get install php5-fpm"); 
+passthru("apt-get install -y php5-cli php5-cgi build-essential libpcre3 libpcre3-dev libpcrecpp0 libssl-dev zlib1g-dev wget");
+#add support for php5-fpm 
+passthru("apt-get install -y php5-fpm"); 
 passthru("mkdir -p /opt/build/nginx");
 chdir("/opt/build/nginx");
 passthru("wget --continue  http://nginx.org/download/".NGINX_VERSION.".tar.gz");
