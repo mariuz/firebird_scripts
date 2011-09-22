5326bef91ac5ddf7e1bf0ce4be70ec326723c351
Popa Marius Adrian <mapopa@gmail.com>
2011-09-22 16:19:20 +0300

    we use php fpm

--- nginx-build.php
+++ nginx-build.php
@@ -1,14 +1,6 @@
 #!/usr/bin/php
 <?
 DEFINE(NGINX_VERSION,'nginx-1.0.6');
-DEFINE(SPAWN_FCGI_VERSION,'spawn-fcgi-1.6.3');
-
-$fp = fopen('/usr/bin/php-fastcgi','w');
-fwrite($fp,"#!/bin/sh\n");
-fwrite($fp,"PHP_FCGI_CHILDREN=10\n");
-fwrite($fp,"PHP_FCGI_MAX_REQUESTS=1000\n");
-fwrite($fp,"/usr/bin/spawn-fcgi -C 10 -F 10 -a 127.0.0.1 -p 9000 -u www-data -f /usr/bin/php5-cgi\n");
-fclose($fp);
 
 passthru("chmod +x /usr/bin/php-fastcgi");
 passthru("mkdir -p /opt/nginx/conf");
@@ -22,7 +14,7 @@ passthru("update-rc.d nginx defaults");
 
 
 
-passthru("apt-get install -y php5-cli php5-cgi build-essential libpcre3 libpcre3-dev libpcrecpp0 libssl-dev zlib1g-dev wget"); 
+passthru("apt-get install -y php5-cli php5-cgi build-essential libpcre3 libpcre3-dev libpcrecpp0 libssl-dev zlib1g-dev wget apt-get install php5-fpm"); 
 passthru("mkdir -p /opt/build/nginx");
 chdir("/opt/build/nginx");
 passthru("wget --continue  http://nginx.org/download/".NGINX_VERSION.".tar.gz");
@@ -32,21 +24,5 @@ passthru("./configure --prefix=/opt/nginx");
 passthru("make");
 passthru("make install");
 
-chdir("/opt/build/");
-passthru("wget --continue http://www.lighttpd.net/download/".SPAWN_FCGI_VERSION.".tar.gz");
-passthru("tar -zxf ".SPAWN_FCGI_VERSION.".tar.gz");
-chdir(SPAWN_FCGI_VERSION);
-
-passthru("./configure");
-passthru("make");
-passthru("cp src/spawn-fcgi /usr/bin/spawn-fcgi");
-passthru("/etc/init.d/init-fastcgi start");
-passthru("chown -R www-data.www-data /opt/nginx");
 passthru("/etc/init.d/nginx start");
-
-
-#passthru("git clone git://github.com/mariuz/firetube.git");
-#chdir("/opt/nginx/html");
-
-
 ?>
